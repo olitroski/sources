8f0cc6c8733c1eea742fb968e62a4b6ef110adfd
Oliver Rojas <olitroski@gmail.com>
2018-05-31 17:12:15 -0400

    se agrega un sources
    Para que cargue todo

---
+++ exploratory/readme.md
@@ -0,0 +1,7 @@
+# Sources
+Varios para uso cotidiano otros para cosas más específicas.
+
+La dirección del sources.r que carga lo más utilizado es 
+`"https://raw.githubusercontent.com/olitroski/sources/master/exploratory/"`
+
+Eso =)
---
+++ exploratory/sources.r
@@ -0,0 +1,20 @@
+# Script para cargar los sources que mas uso.
+
+# Ordenar variables
+source("https://raw.githubusercontent.com/olitroski/sources/master/exploratory/source_order.var.r")
+
+# correlaciones
+source("https://raw.githubusercontent.com/olitroski/sources/master/exploratory/source_pwcorr.r")
+
+# Summary mean y sd 
+source("https://raw.githubusercontent.com/olitroski/sources/master/exploratory/source_summ.r")
+
+# ttest
+source("https://raw.githubusercontent.com/olitroski/sources/master/exploratory/source_ttest_indep.r")
+source("https://raw.githubusercontent.com/olitroski/sources/master/exploratory/source_ttest_pair.r")
+
+
+
+
+
+
